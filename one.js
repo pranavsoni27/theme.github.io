c8d2b836ef3ce4a260b011d486fa130c3e5c1b73
Pranav Soni <130215829+pranavsoni27@users.noreply.github.com>
2024-02-13 15:01:34 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currentMode = "light";
+
+btn.addEventListener("click" , () => {
+        if(currentMode === "light")
+        {
+                currentMode = "dark";
+                body.classList.add("dark");
+                body.classList.remove("light");
+        }
+        else{
+                currentMode = "light";
+                body.classList.add("light");
+                body.classList.remove("dark");
+        }
+
+        console.log(currentMode);
+})
